8621a0e3ca4e777aa038faa5b45f46e942e6126e
Tounyyyyy <tonyluu125576@gmail.com>
2024-03-11 21:09:38 +0100

    select box add

--- app.py
+++ app.py
@@ -1,34 +1,32 @@
 import streamlit as st
-import requests
-import time
+from classes.poke_api import PokeAPI
 
-with st.form("PokemonInfoForm", clear_on_submit=True):
+poke_api = PokeAPI()
+total_pokemon = poke_api.get_total_pokemon_count()
+
+with st.form("PokemonInfoForm"):
     st.subheader("Pokémon Info App")
 
-    pokemon_name_or_id = st.text_input("Zadejte jméno nebo ID Pokémona:")
-    submit_button =  st.form_submit_button("Získat informace o Pokémonech")
+    pokemon_list = poke_api.get_pokemon_list(limit=total_pokemon)
+    pokemon_names_ids = [f"ID {pokemon['url'].split('/')[-2]} - {pokemon['name']}" for pokemon in pokemon_list]
+
+    selected_pokemon = st.selectbox("Vyberte pokémona:", pokemon_names_ids)
+    selected_pokemon_id = selected_pokemon.split("ID ")[1].split(" -")[0]
+
+    submit_button = st.form_submit_button("Získat informace o Pokémonech")
 
     if submit_button:
-        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name_or_id}"
-        response = requests.get(url)
-        print(response)
+        pokemon_data = poke_api.get_pokemon_details(selected_pokemon_id)
 
-        if response.status_code == 200:
-            pokemon_data = response.json()
-            
+        if pokemon_data:
             if 'sprites' in pokemon_data and pokemon_data['sprites'] and 'front_default' in pokemon_data['sprites']:
                 st.image(pokemon_data['sprites']['front_default'], width=200)
 
             if 'name' in pokemon_data:
                 st.write(f"Name: {pokemon_data['name']}")
-            else:
-                info_notification = st.info('Zadejte prosím jméno nebo ID Pokémona')
-                time.sleep(2)
-                info_notification.empty()
+
             if 'types' in pokemon_data:
                 type_pokemon = [t['type']['name'] for t in pokemon_data.get('types', [])]
-                st.write(f"Type(s): {', '.join(type_pokemon)}")
+                st.write(f"Type: {', '.join(type_pokemon)}")
         else:
-            warning_notification = st.warning('Pokémon nebyl nalezen')
-            time.sleep(2)
-            warning_notification.empty()
+            st.warning('Pokémon nebyl nalezen')
